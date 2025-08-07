c4ed2c2dc5c74a1cc68a8269631146e0648fa7e0
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-08-08 00:55:52 +0530

    update

--- game/programs/game/src/lib.rs
+++ game/programs/game/src/lib.rs
@@ -3,6 +3,8 @@ use anchor_lang::prelude::*;
 declare_id!("4HFcKqqTHWvCwqT2CL5qmbprKJwCZeXWYNB4VAQEpcYh");
 
 
+
+
 pub const GAME_SEED: &str = "game";
 #[program]
 pub mod game {
@@ -14,9 +16,29 @@ pub mod game {
         game_account.xp = 0;
         game_account.level=0;
         game_account.owner = ctx.accounts.owner.key();
-        game_account.bump = *ctx.bumps.get("game_profile").unwrap();
+        game_account.bump = *ctx.bumps.get("game_profile").ok_or(GameError::BumpNotFound)?;
         Ok(())
     }
+
+    pub fn save_game(ctx:Context<UpdateProfile>,xp:u16,level:u16) -> Result<()> {
+        let game_account = &mut ctx.accounts.game_profile;
+        if ctx.accounts.owner.key() != game_account.owner {
+            return Err(GameError::Unauthorized.into());
+        }
+        game_account.xp += xp;
+        game_account.level = level;
+
+        Ok(())
+    }
+}
+
+
+#[derive(Accounts)]
+pub struct UpdateProfile<'info>{
+    #[account(mut)]
+    pub owner : Signer<'info>,
+    #[account(mut)]
+    pub game_profile: Account<'info, GameProfile>
 }
 
 
@@ -44,4 +66,13 @@ pub struct GameProfile {
     #[max_len(100)]
     pub name: String,
     pub bump: u8,
+}
+
+
+#[error_code]
+pub enum GameError {
+    #[msg("Unauthorized action")]
+    Unauthorized,
+    #[msg("Bump not found")]
+    BumpNotFound,
 }
